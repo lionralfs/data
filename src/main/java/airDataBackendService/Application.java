b96912c0c94dc5f1436c336fb673ce4e47cb0003
Lion Ralfs <lion.ralfs@gmail.com>
2019-06-03 23:06:08 +0200

    made spring work with tomcat (#8)

--- src/main/java/airDataBackendService/Application.java
+++ src/main/java/airDataBackendService/Application.java
@@ -3,10 +3,11 @@ package airDataBackendService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableScheduling
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
